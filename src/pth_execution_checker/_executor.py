2690af1a5700934b10333e0f4621b603bdf23470
Tim Rid <6593626+timrid@users.noreply.github.com>
2025-03-21 18:59:43 +0100

    Added message in the .pth executor

--- src/pth_execution_checker/_executor.py
+++ src/pth_execution_checker/_executor.py
@@ -1,3 +1,5 @@
 # This code should run automatically via site.py when the python interpreter starts.
 import pth_execution_checker
 pth_execution_checker.pth_executed = True
+
+print("Yay! This is a message called from a .pth file :)")
