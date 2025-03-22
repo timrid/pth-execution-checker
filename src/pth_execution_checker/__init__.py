81162a648794e432f93bb2f7efb5ab506e64583b
Tim Rid <6593626+timrid@users.noreply.github.com>
2025-03-22 13:04:23 +0100

    added check if import socket is possible

--- src/pth_execution_checker/__init__.py
+++ src/pth_execution_checker/__init__.py
@@ -1,1 +1,3 @@
 pth_executed = False
+import_socket_possible = False
+import_socket_exception = None
--- src/pth_execution_checker/_executor.py
+++ src/pth_execution_checker/_executor.py
@@ -3,3 +3,11 @@ import pth_execution_checker
 pth_execution_checker.pth_executed = True
 
 print("Yay! This is a message called from a .pth file :)")
+
+try:
+    import socket
+    pth_execution_checker.import_socket_possible = True
+    pth_execution_checker.import_socket_exception = None
+except Exception as e:
+    pth_execution_checker.import_socket_possible = True
+    pth_execution_checker.import_socket_exception = e
